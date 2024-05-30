fdf27ec0f1a5520b20532accffd5e229337a7e98
fernando <josefernandofloresc73@gmail.com>
2024-05-30 09:41:48 -0300

    club

--- src/Ejercicio_45/Ejercicio_45.java
+++ src/Ejercicio_45/Ejercicio_45.java
@@ -1,5 +1,14 @@
 package Ejercicio_45;
 
+import java.util.Scanner;
+
 public class Ejercicio_45 {
+	public static void main(String[] args) {
+		Scanner datos = new Scanner (System.in);
+		
+		int[]  nom_club = {1011, 2367, 8748, 9121, 817, 6423, 2034};
+		int length = nom_club.length;
+		
+	}
 
 }

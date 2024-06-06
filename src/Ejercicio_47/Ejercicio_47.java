fd62b463af7593cc6e8f18a37ac6cc51e344f831
fernando <josefernandofloresc73@gmail.com>
2024-06-06 08:17:12 -0300

    guardar

--- src/Ejercicio_47/Ejercicio_47.java
+++ src/Ejercicio_47/Ejercicio_47.java
@@ -1,23 +1,24 @@
 package Ejercicio_47;
 
 public class Ejercicio_47 {
-	public class EmparejarNumerosConCaracteres {
-	    
-	        
+	public class EmparejarNumerosYCaracteres {
+
+	        // Definir arrays de números y caracteres
 	        int[] numeros = {1, 2, 3, 4, 5};
-	        char[] caracteres = {'A', 'B', 'C', 'D', 'E'};
+	        char[] caracteres = {"A","B","C","D","E"};
 	        
-	        if (numeros.length != caracteres.length) {
-	            System.out.println("Los arrays deben tener el mismo tamaño.");
+	        // Asegurarse de que los arrays tengan la misma longitud
+	        if (numeros.length == caracteres.length) {
+	            System.out.println("Los arrays no tienen la misma longitud.");
 	            return;
 	        }
 	        
-	       
+	        // Emparejar números con caracteres
 	        for (int i = 0; i < numeros.length; i++) {
-	            System.out.println("Número: " + numeros[i] + " - Carácter: " + caracteres[i]);
+	            System.out.println("Número: " + numeros[i] + ", Caracter: " + caracteres[i]);
 	        }
-	    }
-	
+	    
+	}
 
 
 }
